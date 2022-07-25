05748cc9a29d525f34659fb2cd8b7da2f2a67e34
Sam Deane <sam@elegantchaos.com>
2022-07-25 15:01:32 +0100

    Optionally don't generate the Info.plist.

--- Sources/VersionatorTool/VersionatorTool.swift
+++ Sources/VersionatorTool/VersionatorTool.swift
@@ -8,11 +8,21 @@ import Runner
 
 @main struct VersionatorTool {
     static func main() {
-        guard CommandLine.arguments.count == 4 else {
-            fatalError("wrong arguments passed to tool \(CommandLine.arguments)")
+        let all = ProcessInfo.processInfo.arguments
+        let args = all.filter({ !$0.starts(with: "--") })
+        guard args.count > 2 else {
+            let name = URL(fileURLWithPath: args[0]).lastPathComponent
+            print("\n\nUsage: \(name) <options> <root> <path> {<infoPath>}")
+            exit(1)
         }
-                       
-        let root = CommandLine.arguments[1]
+
+        print(all)
+        print("[error]: test")
+        print("[warning]: test")
+        print("[note]: test")
+//        let options = Set(all.filter({ $0.starts(with: "--") }))
+
+        let root = args[1]
         chdir(root)
         
         let runner = Runner(command: "git")
@@ -48,23 +58,25 @@ import Runner
         }
         """
         
-        let path = CommandLine.arguments[2]
+        let path = args[2]
         let data = generatedSwift.data(using: .utf8)
         let outputURL = URL(fileURLWithPath: path)
         try? FileManager.default.createDirectory(at: outputURL.deletingLastPathComponent(), withIntermediateDirectories: true)
         try? data?.write(to: outputURL)
 
-        let infoPath = CommandLine.arguments[3]
-        let info = [
-            "CFBundleVersion": buildNumber,
-            "Commit": commit,
-            "CFBundleShortVersionString": gitVersion,
-            "CFBundleInfoDictionaryVersion": 6.0
-        ] as NSDictionary
-        
-        let infoData = try? PropertyListSerialization.data(fromPropertyList: info, format: .binary, options: 0)
-        let infoURL = URL(fileURLWithPath: infoPath)
-        try? infoData?.write(to: infoURL)
+        if args.count > 3 {
+            let infoPath = args[3]
+            let info = [
+                "CFBundleVersion": buildNumber,
+                "Commit": commit,
+                "CFBundleShortVersionString": gitVersion,
+                "CFBundleInfoDictionaryVersion": 6.0
+            ] as NSDictionary
+            
+            let infoData = try? PropertyListSerialization.data(fromPropertyList: info, format: .binary, options: 0)
+            let infoURL = URL(fileURLWithPath: infoPath)
+            try? infoData?.write(to: infoURL)
+        }
 
     }
 }
