b9d6865e269fd828b8d393b71e87e19dc8a074f7
Sam Deane <sam@elegantchaos.com>
2024-08-27 19:15:38 +0100

    pass in encoding to use

--- Sources/VersionatorTool/String+AsyncSequence.swift
+++ Sources/VersionatorTool/String+AsyncSequence.swift
@@ -1,7 +1,9 @@
+import Foundation
+
 extension String {
   /// Initialise a string from an async sequence of UInt8.
   /// Consumes the entire sequence.
-  public init<T: AsyncSequence>(_ sequence: T) async where T.Element == UInt8 {
+  public init<T: AsyncSequence>(_ sequence: T, encoding: String.Encoding = .utf8) async where T.Element == UInt8 {
     var buffer: [UInt8] = []
     do {
       for try await byte in sequence {
@@ -11,6 +13,6 @@ extension String {
 
     }
 
-    self = String(bytes: buffer, encoding: .utf8) ?? ""
+    self = String(bytes: buffer, encoding: encoding) ?? ""
   }
 }
