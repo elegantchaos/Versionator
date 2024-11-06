17857bc0bbb5a5602bbe92a768d660d244e766df
Sam Deane <sam@elegantchaos.com>
2024-11-06 10:26:11 +0000

    Fixed test examples

--- Tests/VersionatorTests/Resources/Example.out/Version.h
+++ Tests/VersionatorTests/Resources/Example.out/Version.h
@@ -2,6 +2,6 @@
 /// See github.com/elegantchaos/Versionator for more info.
 #define BUILD 4
 #define CURRENT_PROJECT_VERSION 4
-#define COMMIT 
-#define GIT_VERSION "70f8f9a"
-#define GIT_TAG "70f8f9a"
+#define COMMIT "g70f8f9a"
+#define GIT_VERSION "v1.0.0-0-g70f8f9a"
+#define GIT_TAG "v1.0.0"
